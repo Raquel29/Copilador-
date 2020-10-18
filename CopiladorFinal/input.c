90d5d6bd11ae4e8381a74ac7bef697abef261381
Raquel Pinheiro da Costa <raquelpc2901@gmail.com>
2020-10-18 09:20:04 -0300

    Create input.c

---
+++ CopiladorFinal/input.c
@@ -0,0 +1,16 @@
+/* arquivo input.c */
+
+#include <stdio.h>
+#include "input.h"
+
+char look; /* caracter "lookahead */
+char look; /* caracter "lookahead */
+
+/* lê próximo caracter da entrada */
+void nextChar(){
+	look = getchar();
+}	
+/* inicializa o módulo de entrada */
+void initInput(){
+	nextChar();
+}
